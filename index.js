a1229fb8fd8e0f7102c6ae1645332c029a08f090
Oussama Ben Hebbadj <lo_benhebbadj@esi.dz>
2025-07-07 11:16:57 +0100

    templates-html

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ const server = http.createServer((req, res) => {
     res.end('<h1>Page Not Found</h1>');
   }
 });
+
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("Listening to requests on port 8000"); // Server is listening on port 80
+});
